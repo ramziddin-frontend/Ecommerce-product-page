b62e36825e019313489f0db55171d30e51a428d5
ramziddin-frontend <ramziddintursunov82@gmail.com>
2022-12-19 17:23:24 +0500

    Add site header html, styles and js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const modifiers = {
+  siteHeaderCartModalOpen: 'site-header__cart-modal--open',
+};
+
+
+// Shopping cart modal
+const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
+const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
+
+if (elSiteHeaderCartLink) {
+  elSiteHeaderCartLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    elSiteHeaderCartModal.classList.toggle(modifiers.siteHeaderCartModalOpen);
+  });
+}
